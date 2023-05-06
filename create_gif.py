c9135734e4db080b93ae38054fdb4094fd67c670
Alex Beals <alexcbeals@gmail.com>
2023-05-06 13:01:10 -0700

    Beginning of easing code from ChatGPT

--- create_gif.py
+++ create_gif.py
@@ -6,7 +6,8 @@ class Movie(TypedDict):
     name: str
     rating: Optional[float] # out of 5
 
-def parse_letterboxd_history(csv_file: str):
+# Takes in a CSV file and returns the list of movies
+def parse_letterboxd_history(csv_file: str) -> dict[str, list[Movie]]:
     movies: dict[str, list[Movie]] = {}
 
     with open(csv_file, newline='', encoding='utf-8') as file:
@@ -24,41 +25,9 @@ def parse_letterboxd_history(csv_file: str):
                     'name': name,
                     'rating': rating,
                 })
+    return movies
 
-    animation_slices: list[dict[float, list[Movie]]] = []
-
-    current_buckets: dict[float, list[Movie]] = {
-        0.5: [],
-        1: [],
-        1.5: [],
-        2: [],
-        2.5: [],
-        3: [],
-        3.5: [],
-        4: [],
-        4.5: [],
-        5: [],
-    }
-    for key in sorted(movies.keys()):
-        day_list = movies[key]
-        for movie in day_list:
-            rating = movie['rating']
-            if rating is not None:
-                current_buckets[rating].append(movie)
-                animation_slices.append(copy.deepcopy(current_buckets))    
-
-    # Create a list to store frames
-    frames: list[Any] = []
-    for i, slice in enumerate(animation_slices):
-        frames.append(create_image(slice, i + 1))
-       
-
-    # Save the frames as an animated mp4
-    target_duration_seconds = 5
-
-    num_loops = 3
-    imageio.mimsave('animation.mp4', frames * num_loops, fps=round(len(movies) / target_duration_seconds))
-
+# For a current distribution of movies, creates a single Image frame
 def create_image(bucket_info: dict[float, list[Movie]], count: int):
     # Dimensions of the frames and GIF
     width: int = 256 # actually 250 on the website
@@ -117,5 +86,66 @@ def create_image(bucket_info: dict[float, list[Movie]], count: int):
     # Append the frame to the list
     return frame
 
+# Define the easing function
+def ease(t: float) -> float:
+    # Modify this function to customize the easing effect
+    return t  # Linear easing, change to a different easing function as desired
+
+
+# Takes in a list of movies and creates an animation
+def create_and_save_animation(movies: dict[str, list[Movie]]):
+    animation_slices: list[dict[float, list[Movie]]] = []
+
+    current_buckets: dict[float, list[Movie]] = {
+        0.5: [],
+        1: [],
+        1.5: [],
+        2: [],
+        2.5: [],
+        3: [],
+        3.5: [],
+        4: [],
+        4.5: [],
+        5: [],
+    }
+    for key in sorted(movies.keys()):
+        day_list = movies[key]
+        for movie in day_list:
+            rating = movie['rating']
+            if rating is not None:
+                current_buckets[rating].append(movie)
+                animation_slices.append(copy.deepcopy(current_buckets))    
+
+    # Create a list to store frames
+    frames: list[Any] = []
+    for i, slice in enumerate(animation_slices):
+        frames.append(create_image(slice, i + 1))
+
+    # Define the duration range (in seconds)
+    min_duration = 0.1
+    max_duration = 0.5
+    target_duration_seconds = 5
+    fps = round(len(movies) / target_duration_seconds)
+    
+    # Calculate the easing durations for each frame
+    num_frames = len(frames)
+    durations: list[float] = []
+    for i in range(num_frames):
+        t = i / (num_frames - 1)  # Normalized time between 0 and 1
+        eased_t = ease(t)  # Apply easing function
+        duration = min_duration + (max_duration - min_duration) * eased_t
+        durations.append(duration)
+
+    # Duplicate frames based on their durations
+    duplicated_frames: list[Any] = []
+    for frame, duration in zip(frames, durations):
+        num_repetitions = int(duration * fps)
+        duplicated_frames.extend([frame] * num_repetitions)
+
+    # Save the frames as an animated mp4
+    num_loops = 1
+    imageio.mimsave('animation.mp4', frames * num_loops, fps=fps)
+
 # Example usage
-parse_letterboxd_history('export/diary.csv')
+movies = parse_letterboxd_history('export/diary.csv')
+create_and_save_animation(movies)
